8f2a221e25d6ebc8785303e798c8ae21bb056883
kedarnathpc <kedarnathpc2002@gmail.com>
2024-03-04 17:33:58 +0530

    added security group, vpc, subnet, gateway, route table

--- terraform/v1-EC2.tf
+++ terraform/v1-EC2.tf
@@ -6,12 +6,15 @@ resource "aws_instance" "demo-server" {
     ami = "ami-0e670eb768a5fc3d4"
     instance_type = "t2.micro"
     key_name = "demo-keypair"
-    security_groups = [ "demo-sg" ]
+    # security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [ "aws_security_group.demo-sg.id" ]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
 }
 
 resource "aws_security_group" "demo-sg" {
     name = "demo-sg"
     description = "SSH Access"
+    vpc_id = aws_vpc.dpp-vpc.id
 
     ingress {
         description = "ssh access"
@@ -32,4 +35,56 @@ resource "aws_security_group" "demo-sg" {
     tags = {
         name = "ssh-port"
     }
+}
+
+resource "aws_vpc" "dpp-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "dpp-vpc"
+    }
+}
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name = "dpp-public-subnet-01"
+    }
+}
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1b"
+    tags = {
+        Name = "dpp-public-subnet-02"
+    }
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    tags = {
+        Name = "dpp-igw"
+    }
+}
+
+resource "aws_route_table" "dpp-public-rt" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpp-igw.id
+    }
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+    route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+    subnet_id = aws_subnet.dpp-public-subnet-02.id
+    route_table_id = aws_route_table.dpp-public-rt.id
 }
